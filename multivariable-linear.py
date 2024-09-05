cc56751fcfbb0b92899fa0f062921e1c1363e0cc
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-09-05 11:58:05 -0500

    Initial project

--- multivariable-linear.py
+++ multivariable-linear.py
@@ -1,17 +1,47 @@
 import numpy as np
 
-def Hipotesis(X, W) -> np.ndarray:
-    return np.dot(X, W)
+class LinearRegression:
+    def __init__(self, X, Y, alpha, _lambda):
+        self._lambda = _lambda
+        self.X: np.ndarray = X
+        self.Y: np.ndarray = Y
+        self.W = np.random.rand(X.shape[1])
+        self.alpha = alpha
 
+    def Hipotesis(self):
+        return np.dot(self.X, self.W)
+    
+    def Loss(self):
+        n = len(self.Y)
+        return (1/(2*n)) * (np.linalg.norm(self.Y - self.Hipotesis()) ** 2)
+    
+    def Lasso(self):
+        return self.Loss() + (self._lambda * np.sum(np.abs(self.W)))
+    
+    def Ridge(self):
+        return self.Loss() + (self._lambda * np.sum(self.W ** 2))
+    
+    def Derivative(self):
+        n = len(self.Y)
+        return (1/n) * np.dot(self.X.T, self.Hipotesis() - self.Y)
+    
+    def LassoDerivative(self):
+        return self.Derivative() + self._lambda * np.linalg.norm(self.W, 1)
+    
+    def RidgeDerivative(self):
+        return self.Derivative() + 2 * self._lambda * (np.linalg.norm(self.W) ** 2)
+ 
+    def LassoUpdate(self):
+        self.W = self.W - self.alpha * self.LassoDerivative()
 
-def Loss(X, Y: np.ndarray, W) -> np.ndarray:
-    n = len(Y)
-    return (1/(2*n)) * (np.linalg.norm(Y - Hipotesis(X, W)) ** 2)
+    def RidgeUpdate(self):
+        self.W = self.W - self.alpha * self.RidgeDerivative()
 
+    def LassoTrain(self, epochs):
+        for _ in range(epochs):
+            self.LassoUpdate()
 
-def Lasso(X, Y: np.ndarray, W, _lambda):
-    return Loss(X, Y, W) + (_lambda * np.sum(np.abs(W)))
-
-
-def Ridge(X, Y: np.ndarray, W, _lambda):
-    return Loss(X, Y, W) + (_lambda * np.sum(W ** 2))
+    def RidgeTrain(self, epochs):
+        for _ in range(epochs):
+            self.RidgeUpdate()
+    
