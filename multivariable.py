9611be100a464b9419589be8ac8accf64fbc78d3
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-09-10 16:16:31 -0500

    aaaa

--- multivariable.py
+++ multivariable.py
@@ -1,35 +1,37 @@
 import numpy as np
 
 class NoLinearRegression:
-    def __init__(self, X, Y, alpha, _lambda):
+    def __init__(self, X_train, Y_train, alpha, _lambda):
         self._lambda = _lambda
-        self.X: np.ndarray = X
-        self.Y: np.ndarray = Y
-        self.W = np.random.rand(X.shape[1])
+        self.X_train: np.ndarray = X_train
+        self.Y_train: np.ndarray = Y_train
+        self.W = [1] + list(np.random.rand(X_train.shape[1]))
+        print(self.X_train.shape)
+        print(len(self.W))
         self.alpha = alpha
 
-    def Hipotesis(self):
-        return np.dot(self.X, self.W)
+    def H(self, X, W):
+        return np.dot(X, W)
     
     def Loss(self):
-        n = len(self.Y)
-        return (1/(2*n)) * (np.linalg.norm(self.Y - self.Hipotesis()) ** 2)
+        n = len(self.Y_train)
+        return (1/(2*n)) * (np.linalg.norm(self.Y_train - self.H(self.X_train, self.W)) ** 2)
     
     def Lasso(self):
         return self.Loss() + (self._lambda * np.sum(np.abs(self.W)))
     
     def Ridge(self):
-        return self.Loss() + (self._lambda * np.sum(self.W ** 2))
+        return self.Loss() + (self._lambda * np.linalg.norm(self.W) ** 2)
     
     def Derivative(self):
-        n = len(self.Y)
-        return (1/n) * np.dot(self.X.T, self.Hipotesis() - self.Y)
+        n = len(self.Y_train)
+        return (1/n) * np.dot(self.X_train.T, self.Y_train - self.H(self.X_train, self.W))
     
     def LassoDerivative(self):
         return self.Derivative() + self._lambda * np.sum(np.abs(self.W))
     
     def RidgeDerivative(self):
-        return self.Derivative() + 2 * self._lambda * np.sum(self.W ** 2)
+        return self.Derivative() + 2 * (self._lambda * np.linalg.norm(self.W) ** 2)
  
     def LassoUpdate(self):
         self.W = self.W - self.alpha * self.LassoDerivative()
@@ -37,10 +39,25 @@ class NoLinearRegression:
     def RidgeUpdate(self):
         self.W = self.W - self.alpha * self.RidgeDerivative()
 
+    def reset(self):
+        self.W = np.random.rand(self.X_train.shape[1])
+
     def LassoTrain(self, epochs):
+        L = self.Lasso()
+        L_reg = []
         for _ in range(epochs):
             self.LassoUpdate()
+            self.Y_train = self.H(self.X_train, self.W)
+            L = self.Lasso()
+            L_reg.append(L)
+        return L, L_reg
 
     def RidgeTrain(self, epochs):
+        L = self.Ridge()
+        L_reg = []
         for _ in range(epochs):
             self.RidgeUpdate()
+            self.Y_train = self.H(self.X_train, self.W)
+            L = self.Ridge()
+            L_reg.append(L)
+        return L, L_reg
